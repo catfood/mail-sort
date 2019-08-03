7b0f86ecb0925b70479d60e327bbeca136d8ea92
catfood <mark@criticalresults.com>
2019-08-03 14:25:36 -0400

    Add trivial unit test.

---
+++ MailSortTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailSortTest
+{
+    [TestClass]
+    public class UtilityTests
+    {
+        [TestMethod]
+        public void NullTest()
+        {
+            Assert.IsNull(null);
+        }
+    }
+}
