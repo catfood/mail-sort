8916b84062c6f0de7b96dc516822034e87fd8dd6
catfood <mark@criticalresults.com>
2019-09-15 16:07:31 -0400

    Add simple progress and time display.

--- MailSort/Program.cs
+++ MailSort/Program.cs
@@ -19,15 +19,23 @@ namespace MailSort
             var rulesService = serviceProvider.GetService<IRulesService>();
             retrieverService.Open();
             var inbox = retrieverService.GetInbox();
-            retrieverService.Close();
+            int count = 0;
+            var startTime = DateTime.Now;
             foreach (var modelMessage in inbox)
             {
+                if (count % 10 == 0)
+                {
+                    var diff = DateTime.Now - startTime;
+                    Console.WriteLine($"{count} mail items in {diff.TotalSeconds} seconds.");
+                }
                 var actions = rulesService.GetActionsForMessage(modelMessage);
                 foreach (var action in actions)
                 {
                     var actionResult = retrieverService.Execute(action, modelMessage);
                 }
+                count++;
             }
+            retrieverService.Close();
         }
     }
 }
