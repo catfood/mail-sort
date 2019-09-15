8f7a27f1696e468ad6bcfdab92dede19fc4b9d5f
catfood <mark@criticalresults.com>
2019-09-15 16:04:18 -0400

    MailAction args should be a List, not an array.

--- MailSort/Models/MailAction.cs
+++ MailSort/Models/MailAction.cs
@@ -8,7 +8,11 @@ namespace MailSort.Models
     public enum MailActionType { Null, Delete, Move, Copy, Forward, Archive, Report };
     public class MailAction
     {
+        public MailAction()
+        {
+            Args = new List<object>();
+        }
         public MailActionType ActionType { get; set; }
-        public object[] Args { get; set; }
+        public List<object> Args { get; set; }
     }
 }
