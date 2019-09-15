e6ed2fdb7fefa569a6f7c2b07d2f244397ad17aa
catfood <mark@criticalresults.com>
2019-09-15 16:06:17 -0400

    Implemented a MOVE action; also populate the ID and Folder in advance for use of the MOVE action.

--- MailSort/Net/MailRetriever.cs
+++ MailSort/Net/MailRetriever.cs
@@ -36,6 +36,7 @@ namespace MailSort.Net
 
         public MailActionResult Execute(MailAction action, MailModel input)
         {
+            System.Console.WriteLine(input.From);
             switch (action.ActionType)
             {
                 case MailActionType.Null:
@@ -49,6 +50,7 @@ namespace MailSort.Net
                 case MailActionType.Forward:
                     break;
                 case MailActionType.Move:
+                    MoveMessage(input, action.Args.Select(a => a.ToString()));
                     break;
                 case MailActionType.Report:
                     System.Console.WriteLine($"Got an email from {input.From} to {input.To} regarding {input.Subject}");
@@ -57,7 +59,29 @@ namespace MailSort.Net
             return new MailActionResult { Succeeded = true };
         }
 
+        private void showAllFolders(MailKit.IMailFolder input)
+        {
+            input.GetSubfolders().ToList().ForEach(sf => System.Console.WriteLine(sf.FullName));
+        }
 
+        private void MoveMessage(MailModel msg, IEnumerable<string> dest)
+        {
+            System.Console.WriteLine($"Moving message to folder {string.Join("/",dest)}.");
+            var topNamespace = _client.PersonalNamespaces.First(ns => ns.Path == "");
+            var targetFolder = _client.GetFolder(topNamespace);
+            showAllFolders(targetFolder);
+            foreach (string destFolderElementName in dest)
+            {
+                var newTargetFolder = targetFolder.GetSubfolders().FirstOrDefault(f => f.Name.ToLower() == destFolderElementName.ToLower());
+                if (newTargetFolder == null)
+                {
+                    newTargetFolder = targetFolder.Create(destFolderElementName, true);
+                }
+                targetFolder = newTargetFolder ?? throw new System.NotImplementedException();
+            }
+            System.Console.WriteLine($"I found {targetFolder.FullName}.");
+            msg.Folder.MoveTo(msg.ID, targetFolder);
+        }
 
         public IEnumerable<MailModel> GetInbox()
         {
@@ -65,12 +89,16 @@ namespace MailSort.Net
             {
                 // The Inbox folder is always available on all IMAP servers...
                 var inbox = _client.Inbox;
-                inbox.Open(MailKit.FolderAccess.ReadOnly);
+                inbox.Open(MailKit.FolderAccess.ReadWrite);
+                var items = inbox.Fetch(0, -1, MailKit.MessageSummaryItems.UniqueId);
 
-                foreach (var msg in inbox)
+                foreach (var i in items)
                 {
+                    var msg = inbox.GetMessage(i.UniqueId);
                     var newModel = new MailModel
                     {
+                        ID = i.UniqueId,
+                        Folder = inbox,
                         From = (msg.From.First() as MimeKit.MailboxAddress).Address,
                         To = msg.To.Where(t => t is MimeKit.MailboxAddress).Select(t => (t as MimeKit.MailboxAddress).Address).ToList(),
                         Subject = msg.Subject,
