cec044ed0b7f5463bccaa186112b12ef19a5fb4f
catfood <mark@criticalresults.com>
2019-09-15 16:04:49 -0400

    Keep unique ID and link to folder so those things can be operated on later.

--- MailSort/Models/MailModel.cs
+++ MailSort/Models/MailModel.cs
@@ -6,6 +6,8 @@ namespace MailSort.Models
 {
     public class MailModel
     {
+        public MailKit.UniqueId ID { get; set; }
+        public MailKit.IMailFolder Folder { get; set; } // FIXME: doesn't belong in the model
         public string From { get; set; }
         public List<string> To { get; set; }
         public string Subject { get; set; }
