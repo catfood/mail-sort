a28ca8f518e990b72b6512817f6c8e753cc19eee
catfood <mark@criticalresults.com>
2019-09-22 13:03:04 -0400

    Skeleton Business Layer.

---
+++ MailSortBL/RulesData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MailDTO;
+
+namespace MailSortBL
+{
+    public class RulesData
+    {
+        public RulesetDTO Load(string dbFile)
+        {
+            return new RulesetDTO();
+        }
+
+        public void Save(string dbFile, RulesetDTO data)
+        {
+
+        }
+    }
+}
