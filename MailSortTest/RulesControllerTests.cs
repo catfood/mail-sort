93c2589dfcacc779edaa70aa692bae452734ea69
catfood <mark@criticalresults.com>
2019-10-13 13:12:14 -0400

    Blunt tests for new rules repository.

--- MailSortTest/RulesControllerTests.cs
+++ MailSortTest/RulesControllerTests.cs
@@ -3,17 +3,32 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.AspNetCore.Mvc;
+using MailSort.Rules;
 
 namespace MailSortTest
 {
     [TestClass]
     public class RulesControllerTests
     {
+        private MailSortAPI.Controllers.RulesController controller;
+
+        [TestInitialize]
+        public void Startup()
+        {
+            var bl = new MailSortBL.RulesData();
+            controller = new MailSortAPI.Controllers.RulesController(bl);
+        }
+
         [TestMethod]
         public void RulesControllerIsOK()
         {
-            var controller = new MailSortAPI.Controllers.RulesController();
             Assert.IsInstanceOfType(controller.Get(),typeof(MailDTO.RulesetDTO));
+        }
+
+        [TestMethod]
+        public void RulesControllerCallsBL()
+        {
+            var x = controller.Get();
         }
     }
 }
