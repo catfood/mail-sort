5ac1d976f26928414b48967b5fe5dd7e489fbd7e
catfood <mark@criticalresults.com>
2019-10-26 16:09:16 -0400

    Now with Moq tests!

--- MailSortTest/RulesControllerTests.cs
+++ MailSortTest/RulesControllerTests.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.AspNetCore.Mvc;
 using MailSort.Rules;
+using Moq;
 
 namespace MailSortTest
 {
@@ -11,24 +12,32 @@ namespace MailSortTest
     public class RulesControllerTests
     {
         private MailSortAPI.Controllers.RulesController controller;
+        private Mock<MailSortBL.IRulesData> rulesMock;
 
         [TestInitialize]
         public void Startup()
         {
-            var bl = new MailSortBL.RulesData();
-            controller = new MailSortAPI.Controllers.RulesController(bl);
+            var expectedRulesDTO = new MailDTO.RulesetDTO
+            {
+                RulesetData = "Ruleset Data"
+            };
+            rulesMock = new Mock<MailSortBL.IRulesData>();
+            rulesMock.Setup(foo => foo.Load("rules.db")).Returns(expectedRulesDTO);
+            controller = new MailSortAPI.Controllers.RulesController(rulesMock.Object);
         }
 
         [TestMethod]
         public void RulesControllerIsOK()
         {
-            Assert.IsInstanceOfType(controller.Get(),typeof(MailDTO.RulesetDTO));
+            var getResult = controller.Get();
+            Assert.IsInstanceOfType(getResult,typeof(MailDTO.RulesetDTO));
         }
 
         [TestMethod]
         public void RulesControllerCallsBL()
         {
             var x = controller.Get();
+            rulesMock.Verify(foo => foo.Load("rules.db"), Times.AtLeastOnce());
         }
     }
 }
