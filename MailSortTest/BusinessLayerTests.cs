da5018e7f31ce964a74bfbd4282a7c0cfbe30574
catfood <mark@criticalresults.com>
2019-10-27 12:51:44 -0400

    Add basic business layer test. (Note: RulesData is a "repository." Should implement it that way.)

---
+++ MailSortTest/BusinessLayerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailSortTest
+{
+    [TestClass]
+    public class BusinessLayerTests
+    {
+        [TestMethod]
+        public void TestCreateBusinessLayer()
+        {
+            var bl = new MailSortBL.RulesData();
+            var rules = bl.Load("rules.db");
+            bl.Save("delete-rules.db", rules);
+        }
+    }
+}
